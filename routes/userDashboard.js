fa43e9b2ee9937b77544918c6375f381d59fb3d2
ronelvega19 <ronelvega31@gmail.com>
2024-10-14 12:03:27 +0800

    test notif

--- routes/userDashboard.js
+++ routes/userDashboard.js
@@ -22,7 +22,7 @@ async function getGroqChatCompletion(content) {
         role: 'user', content 
       }
     ],
-    model: 'mixtral-8x7b-32768',
+    model: 'llama3-8b-8192',  
     temperature: 0.5,
     max_tokens: 1024,
     top_p: 1,
